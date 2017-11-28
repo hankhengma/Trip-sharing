1e1f10179c6c8f6f572d7b367303b8acb21a9693
hankhengma <hengma@umich.edu>
2017-11-28 13:04:17 -0500

    Update app.js

--- Final/app.js
+++ Final/app.js
@@ -54,5 +54,5 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 app.listen(process.env.PORT, process.env.IP, function(){
-   console.log("The YelpCamp Server Has Started!");
+   console.log("The Trip-sharing Server Has Started!");
 });
